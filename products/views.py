ef141a6e4dcfc82cecb54e0ad98955eed04c5fbc
R-Prince <reneprince93@icloud.com>
2021-02-14 16:26:16 +0000

    Add edit and delete links to products for super users

--- products/views.py
+++ products/views.py
@@ -75,9 +75,9 @@ def add_product(request):
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to add product, please try again')
     else:
@@ -96,9 +96,9 @@ def edit_product(request, product_id):
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES, instance=product)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'Successfully updated product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Failed to update product')
     else:
@@ -111,3 +111,10 @@ def edit_product(request, product_id):
         'product': product,
     }
     return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, f'Product {product.name} deleted!')
+    return redirect(reverse('products'))
